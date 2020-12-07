fa6a87d0d006552e367b54e51658faed75f19d57
Shreya Srivatsan <padfoot2299@gmail.com>
2020-12-07 15:53:55 +0530

    Update applications.php

--- recruiter/applications.php
+++ recruiter/applications.php
@@ -73,6 +73,7 @@ $rows= $stmt->fetchAll(PDO::FETCH_ASSOC);
 						<ul class="links">
 							<li class="active"><a href="applications.php">Applications</a></li>
 							<li><a href="profile.php">Profile</a></li>
+							<li><a href="finalshortlists.html">Shortlists</a></li>							
 						</ul>
 					</nav>
 
