2810a4a404d5bbbec56378062f4ea9eade4eb854
Shreya Srivatsan <padfoot2299@gmail.com>
2021-04-08 12:58:13 +0530

    update checks

--- tnpcell/editstudent.php
+++ tnpcell/editstudent.php
@@ -145,10 +145,10 @@ if (isset($_POST['submit'])) {
 						Year<input type="text" readonly name="year" id="year" value="<?=$year?>" />
 					</div>
 					<div class="col-3 col-12-xsmall">
-						Active Backlogs<input type="text" pattern="^[0-49]{50}$" name="aback" id="activeback" value="<?=$activeBack?>">
+						Active Backlogs<input type="text" pattern="^[0-9]+$" name="aback" id="activeback" value="<?=$activeBack?>">
 					</div>
 					<div class="col-3 col-12-xsmall">
-						Dead Backlogs<input type="text" pattern="^[0-49]{50}$" name="dback" id="deadback" value="<?=$deadBack?>">
+						Dead Backlogs<input type="text" pattern="^[0-9]+$" name="dback" id="deadback" value="<?=$deadBack?>">
 					</div>
 
 					<div class="col-6">
